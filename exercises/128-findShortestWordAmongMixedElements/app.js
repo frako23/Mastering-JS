13f0581a6479d8e7e67e737dba2f10bed3b64f47
frako23 <frako23@gmail.com>
2023-04-14 15:59:56 +0000

    se agrega el 128

--- exercises/128-findShortestWordAmongMixedElements/app.js
+++ exercises/128-findShortestWordAmongMixedElements/app.js
@@ -1,5 +1,23 @@
 function findShortestWordAmongMixedElements(arr) {
     // your code here
+    if (arr.length == 0) {
+        return ""
+    } 
+    let c = arr.filter(i => typeof i == "string")
+    if (c.length == 0 ) {
+        return ""
+    }
+    let a = c[0];
+    let b = a.length;
+    for (const iterator of c) {
+
+        console.log(a);
+        if (iterator.length < b) {
+            b = iterator.length
+            a = iterator;
+        } 
+    }
+    return a
 }
-let output = findShortestWordAmongMixedElements([4, 'two', 2, 'three']);
+let output = findShortestWordAmongMixedElements([]);
 console.log(output); // --> 'two'
